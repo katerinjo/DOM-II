80e219f27106ea4c2da03b6b185d10390c0d1670
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-09 13:32:37 -0700

    container growth and reset

--- js/index.js
+++ js/index.js
@@ -5,3 +5,28 @@ const debugLog = (...anything) => document.querySelector('.logo-heading').textCo
 const allImgs = document.querySelectorAll('img')
 
 allImgs.forEach(img => img.addEventListener('mouseenter', e => e.target.setAttribute('style', 'visibility: hidden')))
+
+function Stretchy(element, styleFun, start) {
+    let count = start
+    function update() {
+        element.style = styleFun(count)
+    }
+    function change(amt) {
+        count += amt
+        update()
+    }
+    function selectCount() {
+        return count
+    }
+    function set(amt) {
+        count = amt
+        update()
+    }
+    update()
+    return { change, selectCount, set }
+}
+
+const stretchyContainer = Stretchy(document.querySelector('.container.home'), n => `max-width: ${n}px`, 800)
+
+document.addEventListener('keydown', () => stretchyContainer.set(800))
+document.querySelector('h1').addEventListener('click', () => stretchyContainer.change(7))
