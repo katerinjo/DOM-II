10e24e3585d883f795507487783640675a30f582
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-09 12:42:35 -0700

    disappearing images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const debugLog = (...anything) => document.querySelector('.logo-heading').textContent = anything
+
+const allImgs = document.querySelectorAll('img')
+
+allImgs.forEach(img => img.addEventListener('mouseenter', e => e.target.setAttribute('style', 'visibility: hidden')))
